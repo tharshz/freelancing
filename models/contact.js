2ebc88af440d503b9fd46c94aac611f79bebe647
Tharsha Loganathan <tharshzlog@gmail.com>
2020-10-06 12:28:43 +0530

    first commit

---
+++ models/contact.js
@@ -0,0 +1,38 @@
+// const mongoose = require('mongoose');
+// const ContactSchema = new mongoose.Schema({
+//   user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'user'
+//   },
+//      fullname:{
+//          type:String,
+//          required:true
+//      },
+//      phonenumber:{
+//          type:String,
+//          required:true
+//      },
+
+//      email:{
+//          type:String,
+//          required:true,
+//          unique:true
+//      },
+//      title: {
+//         type: String,
+//         required: true
+//       },
+//       description: {
+//         type: String,
+//         required: true
+//       },
+//       date: {
+//         type: Date,
+//         default: Date.now
+//       } 
+
+
+
+// });
+
+// module.exports = mongoose.model('Contact', ContactSchema);
---
+++ server.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+// new1...............................
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+
+const app=express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+
+app.get('/', ()=>{
+    res.send('welcome to my forma');
+})
+
+app.post('/api/forma', (req,res)=>{
+
+let data=req.body;
+
+let smtpTransport = nodemailer.createTransport({
+   service:'Gmail' ,
+   port:465,
+   auth:{
+    user:'tharshz1607@gmail.com',
+    pass:"anutharshz???"
+   }
+});
+
+
+let mailOptions={
+    from:'tharshz1607@gmail.com',
+    to:'tharshz1607@gmail.com',
+    subject:`Message from ${data.name}`,
+    html:`
+    
+    <h3>Informations</h3>
+    <ul>
+      <li>Name: ${data.name}</li>
+      <li>Lastname: ${data.lastname}</li>
+      <li>Email: ${data.email}</li>
+    
+    </ul> 
+    <h3>Message</h3> 
+    <p>${data.message}</p>  
+    
+    `
+
+};
+
+smtpTransport.sendMail(mailOptions, (error, response)=>{
+  
+    if(error){
+        res.send(error)
+    }
+    else{
+        res.send('Success')
+    }
+
+    smtpTransport.close();
+})
+
+
+
+
+
+})
+
+//....................................
+// const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+//app.use('/api/contact', require('./routes/api/contact'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
