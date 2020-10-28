2c542ddeea54dd4e33d60743cf7e2ca1b3a02cc9
Tharsha Loganathan <tharshzlog@gmail.com>
2020-10-29 04:26:50 +0530

    make it

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -54,7 +54,7 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
       {isAuthenticated ? authLinks : guestLinks}
       </div>
     </nav>
